369da3b52de5d8dfa2a8e9c276a398a5747b47c8
Victoria Ancalaf <victoria.ancalaf@gmail.com>
2020-09-03 09:14:35 -0400

    Menu buttons

--- src/components/CakeShop.jsx
+++ src/components/CakeShop.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "../styles/BtnMenu.css";
 
 const CakeShop = () => {
-  return <button className="btnCake">PASTELERIA</button>;
+  return <button className="Cake">PASTELERIA</button>;
 };
 
 export default CakeShop;
