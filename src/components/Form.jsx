aa088baafe65a402894db2e457b7ff5b2b2dec61
JenifferGenoves <jenifer.genoves03@gmail.com>
2020-09-06 20:39:20 -0300

    Form creation

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,85 +1,66 @@
-import React, { useRef } from "react";
-// import "./App.css";
+import React, { Fragment, useState } from 'react';
+import { useRef} from 'react';
 
-const Form = ({ setParentState, parentState }) => {
-  const selector = useRef(null);
-  const [state, setState] = React.useState({
-    waiterName: "",
-    table: "",
-  });
-  const handleChange = ({ target: { value, name } }) => {
-    setState({
-      ...state,
-      [name]: value,
+const Form = () => {
+    const selector = useRef(null);
+    const [data, setData] = useState({
+        waiterName: "",
+        tables: "",
     });
-  };
+    //console.log(data, setData)
+    const handleInputChange = (event) => {
+        setData({
+            ...data,
+            [event.target.name]: event.target.value,
+        });
+    };
+    const sendData = (event) => {
+        event.preventDefault();
 
-  const submit = (event) => {
-    event.preventDefault();
-    setParentState([...parentState, state]);
-    setState({
-      waiterName: "",
-      table: "",
-    });
-    selector.current.selectedIndex = 0;
-  };
-  return (
-    <FormView
-      handleChange={handleChange}
-      submit={submit}
-      waiterName={state.waiterName}
-      selector={selector}
-    />
-  );
-};
-
-/* const DataView = ({ parentState }) => {
-  return (
-    <ol>
-      {Array.isArray(parentState) &&
-        parentState.map(({ waiterName, table }) => (
-          <li>
-            <h1>
-              {waiterName} {table}
-            </h1>
-          </li>
-        ))}
-    </ol>
-  );
-};  */
-
-const FormView = ({ handleChange, submit, waiterName, table, selector }) => (
-  <div className="form">
-    <form onSubmit={submit}>
-      <input
-        value={waiterName}
-        type="text"
-        waiterName="Nombre Mesero"
-        onChange={handleChange}
-      />
-      <select ref={selector} name="N° de Mesa" id="" onChange={handleChange}>
-        <option value="">Mesa</option>
-        <option value="1">1</option>
-        <option value="2">2</option>
-        <option value="3">3</option>
-        <option value="4">4</option>
-        <option value="5">5</option>
-      </select>
-    </form>
-  </div>
-);
-
-/* function App() {
-  const [parentState, setParentState] = useState([]);
-  return (
-    <>
-      <Form setParentState={setParentState} parentState={parentState} />
-      <div>
-        <br />
-        <DataView parentState={parentState} />
-      </div>
-    </>
-  );
-} */
-
+    };
+    return (
+    <Fragment>
+      <div className="item1">
+        <form className="row mt-3" onSubmit={sendData}>
+          <div className="col">
+            <select ref={selector} name="waiterName" id="" onChange={handleInputChange}>
+              <option value="">Mesero</option>
+              <option value="Ignacio">Ignacio</option>
+              <option value="Fernanda">Fernanda</option>
+              </select>
+          </div>
+          <div className="col">
+            <select ref={selector} name="tables" id="" onChange={handleInputChange}>
+              <option value="">N° de Mesa</option>
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+            </select>
+            <div className="col">
+              {/* <button className="btn btn-dark" type="submit">Registrar</button> */}
+            </div>
+          </div>
+        </form>
+        {/* <Router >
+          <div className="container mt-2">
+            <div className="btn-group">
+            <Link to="/Drinks" className="btn btn-dark">Café</Link>
+            <Link to="/CakeShop" className="btn btn-dark">Pastelería</Link>
+            </div>
+          <Switch>
+            <Route path="/Drinks" exact>
+              <Drinks name={data.waiterName} tables={data.tables} />
+            </Route>
+            <Route path="/CakeShop" exact>
+            <CakeShop name={data.waiterName} tables={data.tables} />
+            </Route>
+            </Switch>
+            </div>
+            </Router>*/}
+          </div>
+    </Fragment>
+    )
+}
 export default Form;
