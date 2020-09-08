16732aba358f1a64b9a2f567d3cf74e9f9d835d3
PamelaBarboza <pamelarabarboza@gmail.com>
2020-09-08 19:11:33 -0300

    Add cart

--- src/components/Trash.jsx
+++ src/components/Trash.jsx
@@ -1,12 +1,13 @@
-import React from 'react'
-import TrashCan from '../img/basura.png'
+/* eslint-disable jsx-a11y/alt-text */
+import React from "react";
+import TrashCan from "../img/basura.png";
 
 const Trash = () => {
-    return (
-        <div className="btn-transh">
-            <img src={TrashCan} />
-        </div>
-    )
-}
+  return (
+    <div className="btn-transh">
+      <img src={TrashCan} />
+    </div>
+  );
+};
 
 export default Trash;
