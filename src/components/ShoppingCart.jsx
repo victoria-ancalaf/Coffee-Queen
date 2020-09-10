d43a929f86366011561c511ebe0c3b65b230ec0a
JenifferGenoves <jenifer.genoves03@gmail.com>
2020-09-10 11:36:36 -0300

    changes css

--- src/components/ShoppingCart.jsx
+++ src/components/ShoppingCart.jsx
@@ -7,6 +7,7 @@ import ButtonsOrder from "../components/ButtonsOrder";
 import Trash from "../components/Trash";
 import { firebase } from "../firebase";
 import { Link } from "react-router-dom";
+import DataView from "../components/DataView"
 
 const db = firebase.firestore();
 //console.log(db);
@@ -53,6 +54,7 @@ const ShoppingCart = ({ order, handleDelete }) => {
     <div className="containerCart">
       <div className="Tables">
       <Form />
+      <DataView />
       </div>
       <div className="StylesTitle">
         <p>PRODUCTOS</p>
