77b52b843772d51e1e7678b30a5f4388acd82140
PamelaBarboza <pamelarabarboza@gmail.com>
2020-09-11 18:50:37 -0300

    Status

--- src/Views/FoodOrders.js
+++ src/Views/FoodOrders.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "../components/Header";
 import OrderCheck from "../components/OrderCheck";
+import "../styles/orderCheck.css";
 
 function Waiter() {
   return (
